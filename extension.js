2aa41121b4a593907fe17de8592f11d225fe601c
Lucas <lucasvalenteds@gmail.com>
2021-07-09 12:45:12 -0300

    Creating I18n class and refactoring code to use it

--- extension.js
+++ extension.js
@@ -5,30 +5,52 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
-const GETTEXT_DOMAIN = 'my-indicator-extension';
-const Gettext = imports.gettext.domain(GETTEXT_DOMAIN);
-const _ = Gettext.gettext;
-
 class ThemeVariantIndicator extends PanelMenu.Button {
 
+    constructor(i18n) {
+	this.i18n = i18n;
+    }
+
     _init() {
-	super._init(0.0, _("indicator-name"));
+	super._init(0.0, this.i18n.getText("indicator-name"));
 
 	this.add_child(new Style.Icon({
 	    icon_name: 'face-smile-symbolic',
 	    style_class: 'system-status-icon',
 	}));
 
-	const darkThemeItem = new PopupMenu.PopupMenuItem(_("indicator-menu-item-label-dark"));
-	darkThemeItem.connect("activate", this.change);
-	this.menu.addMenuItem(darkThemeItem);
+	this.menu.addMenuItem(this.createMenuItemDark());
+	this.menu.addMenuItem(this.createMenuItemLight());
+    }
 
-	const lightThemeItem = new PopupMenu.PopupMenuItem(_("indicator-menu-item-label-light"));
-	lightThemeItem.connect("activate", this.change);
-	this.menu.addMenuItem(lightThemeItem);
+    createMenuItemDark() {
+	const label = this.i18n.getText("indicator-menu-item-label-dark")
+	const item = new PopupMenu.PopupMenuItem(label);
+	item.connect("activate", this.change);
+	return item;
+    }
+
+    createMenuItemLight() {
+	const label = this.i18n.getText("indicator-menu-item-label-light")
+	const item = new PopupMenu.PopupMenuItem(label);
+	item.connect("activate", this.change);
+	return item;
     }
 
     change() {
+    }
+}
+
+const ExtensionGetText = imports.gettext;
+
+class I18n {
+
+    constructor(domain) {
+	this.domain= ExtensionGetText.domain(domain);
+    }
+
+    getText(key) {
+	return this.domain.gettext(key);
     }
 }
 
@@ -37,19 +59,22 @@ const ExtensionUtils = imports.misc.extensionUtils;
 
 class Extension {
 
-    constructor(currentExtension) {
-	this.uuid = currentExtension.meta.uuid;
-	ExtensionUtils.initTranslations(this.uuid);
+    constructor(extension) {
+	this.domain = extension.meta.uuid;
+	ExtensionUtils.initTranslations(this.domain);
     }
 
     enable() {
-	this.indicator = new ExtensionIndicator();
-	Main.panel.addToStatusArea(this.uuid, this.indicator);
+	this.i18n = new I18n(this.domain);
+	this.indicator = new ExtensionIndicator(this.i18n);
+
+	Main.panel.addToStatusArea(this.domain, this.indicator);
     }
 
     disable() {
 	this.indicator.destroy();
 	this.indicator = null;
+	this.i18n = null;
     }
 }
 
